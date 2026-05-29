f88489a87738e880135588966392dcfd83b71909
Taenam Kim <tkim356@wisc.edu>
2026-05-29 11:08:53 -0400

    export HTMLParser from parsers package

--- src/redactify/parsers/__init__.py
+++ src/redactify/parsers/__init__.py
@@ -1,11 +1,13 @@
 """Document parsers package."""
 
 from redactify.parsers.base import BaseParser, DocumentChunk, ParsedDocument
+from redactify.parsers.html import HTMLParser
 from redactify.parsers.text import TextParser
 
 __all__ = [
     "BaseParser",
     "DocumentChunk",
+    "HTMLParser",
     "ParsedDocument",
     "TextParser",
 ]
