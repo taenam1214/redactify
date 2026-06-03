7e90ccd112ac893cd8159b05311261c50678fa8a
Taenam Kim <tkim356@wisc.edu>
2026-06-03 11:43:46 -0400

    export AuditTrail in public API

--- src/redactify/__init__.py
+++ src/redactify/__init__.py
@@ -2,11 +2,13 @@
 
 __version__ = "0.1.0"
 
+from redactify.core.audit import AuditTrail
 from redactify.core.detector import PIIEntity, PIIType
 from redactify.core.engine import RedactionEngine
 from redactify.core.redactor import RedactionMode, Redactor
 
 __all__ = [
+    "AuditTrail",
     "PIIEntity",
     "PIIType",
     "RedactionEngine",
